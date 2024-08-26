f9162d2f2ebbd6595e312bd8e2f3ef418c7c9b4d
tounektimohamed <87899355+tounektimohamed@users.noreply.github.com>
2024-08-26 21:02:36 +0100

    Update app.py
    z to zero

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pyproj
 import json
-from shapely.geometry import shape, mapping
+from shapely.geometry import shape, mapping, LineString, Polygon
 from shapely.ops import transform
 
 app = Flask(__name__)
@@ -15,14 +15,25 @@ def convert_utm_to_wgs84(geometry, epsg_code):
         transformer = pyproj.Transformer.from_proj(proj_utm, proj_wgs84, always_xy=True)
 
         def project(x, y, z=None):
-            if z is None:
-                return transformer.transform(x, y)
+            lon, lat = transformer.transform(x, y)
+            return lon, lat
+
+        def transform_geom(geom):
+            def process_coords(coords):
+                # Transform coordinates and set Z to zero
+                return [(lon, lat, 0) if len(coord) == 3 else (lon, lat) for coord in coords]
+
+            # Handle 3D geometries by setting Z to zero
+            if geom.has_z:
+                transformed_geom = transform(lambda x, y, z=None: project(x, y), geom)
+                # Update coordinates to have Z as zero
+                transformed_geom = geom.__class__(process_coords(transformed_geom.coords), geom.exterior if isinstance(geom, Polygon) else None)
             else:
-                lon, lat = transformer.transform(x, y)
-                return lon, lat, z
+                transformed_geom = transform(lambda x, y: project(x, y), geom)
+            
+            return transformed_geom
 
-        transformed_geom = transform(project, shape(geometry))
-
+        transformed_geom = transform_geom(shape(geometry))
         return transformed_geom
     except Exception as e:
         raise Exception(f"Erreur de conversion UTM à WGS84: {e}")
